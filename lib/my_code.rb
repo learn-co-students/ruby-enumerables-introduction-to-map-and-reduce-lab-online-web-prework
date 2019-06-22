ddf1958388f44d6570f3b2544c73b9243db492c7
Dominique De León <dominique.a.deleon@gmail.com>
2019-06-22 19:50:15 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,3 +10,13 @@ def map_to_negativize(arr)
   new_arr
 end
 
+def map_to_no_change(arr)
+ # arr.map {|n| n}
+  new_arr = []
+  i = 0
+  while i < arr.length
+    new_arr << arr[i]
+    i += 1
+  end
+  new_arr
+end
