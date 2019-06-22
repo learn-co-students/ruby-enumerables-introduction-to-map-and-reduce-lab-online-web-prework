d1d014a86ef229218465f3ec3abce0332e2a3ad8
Dominique De León <dominique.a.deleon@gmail.com>
2019-06-22 19:59:51 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -31,3 +31,34 @@ def map_to_double(arr)
   end
   new_arr
 end  
+
+def map_to_square(arr)
+  #arr.map {|n| n**2}
+  new_arr = []
+  i = 0
+  while i < arr.length
+    new_arr << arr[i]**2
+    i +=1
+  end
+  new_arr
+end
+
+def reduce_to_total(arr, start=0)
+  i = 0
+  while i < arr.length
+    start += arr[i]
+    i +=1
+  end 
+  start
+end
+
+def reduce_to_all_true(arr)
+  #arr.all? {|el| el}
+  #!arr.any? {|el| !el}
+  i = 0
+  while i < arr.length
+    return false if !arr[i]
+    i += 1
+  end
+  true
+end
