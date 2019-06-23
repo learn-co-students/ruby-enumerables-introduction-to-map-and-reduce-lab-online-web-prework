c9753c26f4417c795751c68fbff0fc80ef4472e0
Indu Ramesh <induramesh@gmail.com>
2019-06-23 21:52:21 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,28 @@
 # My Code here....
+def map_to_negativize(arr)
+  arr.map {|i| (i * -1)}
+end 
+
+def map_to_no_change(arr)
+  arr.map{|i| i}
+end 
+
+def map_to_double(arr)
+  arr.map{|i| (2*i)}
+end 
+
+def map_to_square(arr)
+  arr.map{|i| (i**2)}
+end 
+
+def reduce_to_total(arr, starting_point = 0)
+  starting_point + arr.reduce(:+)
+end
+
+def reduce_to_all_true(arr) 
+  arr.all? 
+end 
+
+def reduce_to_any_true(arr) 
+  arr.any? 
+end 
