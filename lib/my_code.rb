68098a8286cd22f7be6e9723a4c68975dfa8501d
Clara Yip <clara.yip1@gmail.com>
2019-07-02 22:42:01 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,71 +1,62 @@
 # My Code here....
 def map_to_negativize(array)
-  i = 0
   new_array = Array.new
-  while i < array.length do 
-  y = array[i]
-  new_array.push(y * -1)
-  i += 1
+  array.each do |i|
+  new_array.push(i * -1)
   end
-  return new_array
+  new_array
 end
 
 def map_to_no_change(array)
-  array
+  new_array = Array.new 
+  array.each do |i|
+    new_array.push(i)
+  end
+  new_array
 end
 
 def map_to_double(array)
-  i = 0
-  doubled_array = Array.new
-  while i < array.length do 
-  doubled_array << (array[i] * 2)
-  i += 1
+  doubled_array = []
+  array.each do |i|
+  doubled_array << (i * 2)
   end
-  return doubled_array
+  doubled_array
 end
 
 def map_to_square(array)
-  i = 0 
   squared_array = []
-  while i < array.length do 
-  squared_array.push(array[i] ** 2)
-  i += 1 
+  array.each do |i|
+  squared_array.push(i ** 2)
   end
-  return squared_array
+  squared_array
 end
 
 def reduce_to_total(source_array, starting_point = 0)
-  i = 0
   total = starting_point
-  while i < source_array.length
-    total += source_array[i]
-    i += 1
+  source_array.each do |i|
+    total += i
   end 
   total
 end 
 
 def reduce_to_all_true(source_array)
-  i = 0 
   reduction = true 
-  while i < source_array.length do
-    if source_array[i]
-    reduction = true
+  source_array.each do |i|
+    if i
+    reduction
     else 
-    return reduction = false
+    reduction = false
     end
-    i += 1
   end
   reduction
 end
 
 def reduce_to_any_true(source_array)
-  i = 0 
   reduction = false
-  while i < source_array.length do
-    if source_array[i]
+  source_array.each do |i|
+    if i
     reduction = true
     end
-    i += 1
   end
   reduction
 end
