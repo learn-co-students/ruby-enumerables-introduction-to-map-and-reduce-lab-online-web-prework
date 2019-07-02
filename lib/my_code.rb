c91c5629300a0d134e940db4630c1f8219f824b9
Clara Yip <clara.yip1@gmail.com>
2019-07-02 01:04:35 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,11 @@
 # My Code here....
+def map_to_negativize(array)
+  i = 0
+  new_array = Array.new
+  while i < array.length do 
+  y = array[i]
+  new_array << (y * -1)
+  i += 1
+  end
+  return new_array
+end 
