e339a090ecc46f7e7f467ddc4d02019d53903fb6
Clara Yip <clara.yip1@gmail.com>
2019-07-02 01:27:17 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,9 +3,32 @@ def map_to_negativize(array)
   i = 0
   new_array = Array.new
   while i < array.length do 
-  y = array[i]
-  new_array << (y * -1)
+  new_array << (array[i]* -1)
   i += 1
   end
   return new_array
-end 
+end
+
+def map_to_no_change(array)
+  array
+end
+
+def map_to_double(array)
+  i = 0
+  doubled_array = Array.new
+  while i < array.length do 
+  doubled_array << (array[i] * 2)
+  i += 1
+  end
+  return doubled_array
+end
+
+def map_to_square(array)
+  i = 0 
+  squared_array = []
+  while i < array.length do 
+  squared_array.push(array[i] ** 2)
+  i += 1 
+  end
+  return squared_array
+end
