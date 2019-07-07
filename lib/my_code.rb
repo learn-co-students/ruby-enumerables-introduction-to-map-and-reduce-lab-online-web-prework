6780395f2791a628b8bbf67f62b37ea364d9e9b0
assane savadogo <assane.savadogo81@gmail.com>
2019-07-07 14:04:35 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,34 @@
-# My Code here....
+require 'pry'
+def map_to_negativize(source_array)
+  source_array.map { |num| - num }
+end
+
+def map_to_no_change(source_array)
+  source_array.map { |num| num }
+end
+
+def map_to_double(source_array)
+  source_array.map { |num| num * 2 }
+end
+
+def map_to_square(source_array)
+  source_array.map { |num| num ** 2 }
+end
+
+def reduce_to_total(source_array, starting_point = 0)
+   source_array.reduce(starting_point) { |num, x| num + x }
+end
+
+
+def reduce_to_all_true(source_array)
+  i = 0 
+  while i < 3 do
+      #binding.pry
+      if !source_array[i] 
+       return false 
+       i = i + 1
+         #binding.pry
+     
+    end
+  end
+end
