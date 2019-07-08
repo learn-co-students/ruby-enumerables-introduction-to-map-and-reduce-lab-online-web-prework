e994d20e494d87ee530888c2c2263a8b39e0b80f
Katie Seggelke <katie.seggelke@gmail.com>
2019-07-08 19:24:28 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,66 @@
-# My Code here....
+def map_to_negativize(source_array)
+  i = 0 
+  new_array = []
+  while i < source_array.length do 
+    new_array << source_array[i] * -1 
+    i += 1 
+  end 
+  return new_array
+end
+
+def map_to_no_change(source_array)
+  return source_array
+end
+
+def map_to_double(source_array)
+  i = 0 
+  new_array = []
+  while i < source_array.length do  
+    new_array << source_array[i] * 2 
+  i += 1 
+  end
+  return new_array
+end
+
+def map_to_square(source_array)
+  i = 0 
+  new_array = []
+  while i < source_array.length do 
+    new_array << source_array[i] ** 2 
+    i += 1 
+  end
+  return new_array
+end
+
+def reduce_to_total(source_array, starting_point = 0)
+  i = 0 
+  while i < source_array.length do 
+    total = source_array.inject(starting_point) {|sum, i| sum + i}
+    i += 1 
+  end 
+  return total 
+end 
+
+def reduce_to_all_true(source_array)
+  i = 0 
+  while i < source_array.length do 
+    if source_array[i] == FALSE
+      return false
+    end 
+    i += 1 
+  end 
+  true 
+end 
+
+def reduce_to_any_true(source_array)
+  i = 0 
+  while i < source_array.length do 
+    if source_array[i] == true 
+      return true
+    end 
+    i += 1 
+  end 
+    false 
+end 
+
+
