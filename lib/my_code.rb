24da1b66c7650108fb7d20d57a0eb9dc49e3db6b
Ines Angelica Thompson <i.thompson89@outlook.com>
2019-07-13 00:52:14 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,30 @@
-# My Code here....
+def map_to_negativize(source_array)
+  return source_array.map{|i|
+    i * -1
+  }
+end
+
+def map_to_no_change(arr)
+  return arr.map{|i| i}
+end
+
+def map_to_double(arr)
+  return arr.map{|i| i * 2}
+end
+
+def map_to_square(arr)
+  return arr.map{|i| i * i}
+end
+
+def reduce_to_total(arr, start = 0)
+  return arr.reduce(start) { |prev, cur| prev + cur }
+end
+
+def reduce_to_all_true(arr)
+  return arr.reduce(true) {|prev, cur| prev && cur }
+end
+
+def reduce_to_any_true(arr)
+  return arr.reduce(0) {|prev, cur| prev += !!cur ? 1 : 0 } > 0
+end
+
