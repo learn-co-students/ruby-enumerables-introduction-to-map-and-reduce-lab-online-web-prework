23c024bd7d7939f1b3609abfebdd926b82977723
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-07-24 03:49:47 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,10 @@
-# My Code here....
+(map_to_nagativize([1, ,2, 3, -9])) .to eq([-1, -2, -3, 9])
+(map_to_change(dune)) .to eq (dune)
+(map_to_double([1, 2, 3, -9]))) .to eq([2, 4, 6, -18])
+(map_to_square([1, 2, 3, -9]))) .to eq([1, 4, 9, 81])
+(reduce_to_total(source_array) .to eq(6)
+(reduce_to_total(source_array, starting_point)) .to eq (106)
+(reduce_to_all_true(source_array)) .to be_truthy
+(reduce_to_all_true(source_array)) .to be_falsy
+(reduce_to_all_true(source_array)) .to eq(true)
+(reduce_to_all_true(source_array)) .to eq(false)
