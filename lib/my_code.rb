1905b5d5b7f849671221e7f1cd8d7c83bfa90fb0
Joey Luis Echevarria <jechevarria719@gmail.com>
2019-08-04 16:33:08 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -6,3 +6,7 @@ describe 'my own map-like methods'
       expect(map_to_negativize(['1','2','3','-9'])).to eq(['-1','-2','-3','9'])
     end
   end
+
+describe "map_to_no_change returns an array with the original values"
+  it "transforms correctly"
+  
