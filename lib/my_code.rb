e87a229f14b891ef81396af3f435a8042e1360c2
Calvin McDowell <calvinmac40@hotmail.com>
2019-08-06 22:19:57 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,41 +1,34 @@
 def map_to_negativize(source_array)
-  source_array.map { |n| n * -1}
+  source_array.map { |num| num * -1}
 end
 
 def map_to_no_change(source_array)
-  source_array.map { |n| n * 1}
+  source_array.map { |num| num * 1}
 end
 
 def map_to_double(source_array)
-  source_array.map { |n| n * 2}
+  source_array.map { |num| num * 2}
 end
 
 def map_to_square(source_array)
-  source_array.map { |n| n **2}
+  source_array.map { |num| num ** 2}
 end
 
-def reduce_to_total(source_array, starting_point = 0) 
-  source_array.reduce(starting_point) { |sum, num| num + sum}
+def reduce_to_total(source_array, starting_point = 0)   source_array.reduce(starting_point) { |sum, num| sum + num } 
 end
-
-
-def reduce_to_all_true(source_array)
-  sum = 100
-  source_array.reduce { |n| n + sum}
-end
-
 
 def reduce_to_any_true(source_array)
-  source_array.any? { |word| word = 0 }
+  source_array = [1,2,3]
+  source_array.reduce { |num| num === 0 }
+end
+
+def reduce_to_all_true(source_array)
+  source_array = [1 + 1 == 3]
+  source_array.reduce {|sum, num| sum + num }
+end
+
+def reduce_to_any_true(source_array)
+   source_array.any? {|sum| sum }
 end
 
 
-
-
-    
-  
-    
-  
-  
-
-  
