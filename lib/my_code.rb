c0e94c6cb004ae645f5a3ac9d743d1f30c586068
Shaqqour M <mshaqqourm@gmail.com>
2019-08-22 00:08:58 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,28 @@
 # My Code here....
+def map_to_negativize(arr)
+  arr.map { |num| num * -1 }
+end
+
+def map_to_no_change(arr)
+  arr
+end
+
+def map_to_double(arr)
+  arr.map { |num| num * 2 }
+end
+
+def map_to_square(arr)
+  arr.map { |num| num * num }
+end
+
+def reduce_to_total(arr, start = 0)
+  arr.reduce { |sum, num| sum += num } + start
+end
+
+def reduce_to_all_true(arr)
+  arr.reduce { |acc, ele| acc && ele }
+end
+
+def reduce_to_any_true(arr)
+  arr.reduce { |acc, ele| acc || ele } || false
+end
