26239b2fc26927c8caeee1cfe22f72d4a8d98c41
Janay Bailey <tjanay87@gmail.com>
2019-08-27 16:33:38 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,9 @@
 # My Code here....
+def map_to_negativize(-1[1,2,3])
+def map_to_no_change(1[1,2,3])
+def map_to_double(2[1,2,3])  
+def map_to_square(.sqrt[1,2,3])
+  
+def reduce_to_total([])
+def reduce_to_all_true([])
+def reduce_to_any_true([])
