74fb31e6fe81aa3aaeb13e39976882c12415679e
Brianna Jordan <bryvjordan9@gmail.com>
2019-08-29 20:41:28 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,4 +1,23 @@
 # My Code here....
-def map_to_negativize
-  [1, 2, 3, -9].map {|f| f * -1}
+def map_to_negativize (array)
+  array.map {|f| f * -1}
 end
+
+def map_to_no_change (dune)
+  return dune
+end
+
+def map_to_double (array)
+  array.map {|f| f * 2}
+end
+
+def map_to_square (array)
+  array.map {|f| f **2}
+end
+
+
+def reduce_to_total (source_array)
+  source_array.reduce(0) { |sum, element| sum + element }
+  source_array.reduce(100) { |sum, element| sum + element}
+end
+end 
