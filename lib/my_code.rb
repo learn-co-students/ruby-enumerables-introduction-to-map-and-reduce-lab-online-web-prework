5468671b5c11864d4b9e3d7c34743ac45d0f6ff9
Jalysa Reason <jalysareason@gmail.com>
2019-08-30 00:04:59 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,19 @@
-# My Code here....
+def map_to_negativize(source_array)
+  map_to_negativize(source_array) do |index|
+    source_array(1) = |index|
+  end
+    map_to_negativize
+end
+
+def map_to_no_change(source_array)
+  map_to_no_change(source_array) do |index|
+    source_array(1) = |index|
+  end
+  map_to_no_change
+end
+
+def map_to_double(source_array)
+end
+
+def map_to_square(source_array)
+end
