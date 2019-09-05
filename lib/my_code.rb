fbaba519f2f7e310232fec7ed16fa279b6b80b93
Brian Kao <bk1613@nyu.edu>
2019-09-05 16:40:29 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,67 @@
 # My Code here....
+def map_to_negativize(source_array)
+  i = 0
+  while source_array[i] do
+    source_array[i] *= -1
+    i += 1
+  end
+  source_array
+end
+
+def map_to_no_change(source_array)
+  source_array
+end
+
+def map_to_double(source_array)
+  i = 0
+  while source_array[i] do
+    source_array[i] *= 2
+    i += 1
+  end
+  source_array
+end
+
+def map_to_square(source_array)
+  i = 0
+  
+  while source_array[i] do
+    source_array[i] **= 2
+    i += 1
+  end
+  source_array
+end
+
+def reduce_to_total(source_array, starting_point = 0)
+  i = 0
+   
+  while source_array[i] do
+    starting_point += source_array[i]
+    i += 1
+  end
+  
+  starting_point
+end
+
+def reduce_to_all_true(source_array)
+  i = 0
+  while i < source_array.length 
+    if source_array[i] == false 
+      return false
+    end
+    i += 1
+  end
+  return true 
+end
+
+def reduce_to_any_true(source_array)
+  i = 0
+  while i < source_array.length 
+    if source_array[i] == true 
+      return true
+    end
+    i += 1
+  end
+  return false
+
+end
+
