3d4c77b7e1f381da756faae27197f9d1fd926cd2
Wendy Harris <wendyv510@hotmail.com>
2019-09-23 00:08:28 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,30 @@
-# My Code here....
+map_to_negativize [1,2,3,-9]
+
+map_to_no_change
+dune= ["paul","gurner","vladimir","jessica","chani"]
+
+map_to_double[(1,2,3,-9)double]
+
+map_to_square[(1,2,3,-9)*]
+
+reduce_to_total
+     source_array=[1,2,3]
+     starting=0 
+     
+reduce_to_total
+      source_array =[1,2,3]
+      starting_point=100
+      
+reduce_to_all_true
+     source_array=[1,2,true,"razmatazz"]
+     
+reduce_to_all_true
+     source_array=[1,2,true,"razmatazz",false]
+     
+reduce_to_any_true
+     source_array=[false,nil,nil,nil,true]
+     
+reduce_to_any_true
+     source_array=[false,nil,nil,nil]
+     
+     
