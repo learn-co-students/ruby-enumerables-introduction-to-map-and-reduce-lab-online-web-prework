cae10a57287a12ede2779e395c717caa391bc028
Jeffery Eckert <bromodo@gmail.com>
2019-09-23 00:03:46 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,11 @@
-# My Code here....
+def map_to_negativize(array)
+  final_array = []
+  counter = 0 
+   while counter < array.size do
+   final_array.push{array[counter] * -1}
+   counter <= 1 
+   end
+   return final_array
+ end
+
+
