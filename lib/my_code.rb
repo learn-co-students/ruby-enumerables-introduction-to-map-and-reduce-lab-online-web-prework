2da2914b60f4888cd7695ac758c1961bb2e8a116
Laura Hatch <laura.hatch.lh@gmail.com>
2019-09-27 20:14:16 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -58,3 +58,21 @@ while counter < array.size do
 end
 total
 end
+
+def reduce_to_all_true(array)
+  counter = 0
+while counter < array.size do
+  return false if array[counter] == false
+    counter += 1
+end
+return true
+end
+
+def reduce_to_any_true(array)
+  counter = 0
+while counter < array.size do
+  return true if array[counter] == true
+    counter += 1
+end
+return false
+end
