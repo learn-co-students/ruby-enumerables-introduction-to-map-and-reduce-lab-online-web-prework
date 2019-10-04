89f390c5438dcdbfd06fd1ebf35b3ac268f60e70
Derric Fragosa <fderric@gmail.com>
2019-10-04 01:25:01 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,73 @@
+
 # My Code here....
+
+ def map_to_negativize(my_array)
+   new_array = []
+   count = 0 
+   while count < my_array.length do
+   new_array << my_array[count] * -1 
+   count +=1 
+    end 
+ new_array
+ end
+
+def map_to_no_change(kids_array)
+   newkids_array = []
+   count = 0 
+   while count < kids_array.length do 
+    newkids_array << kids_array[count]
+    count +=1
+    
+  end 
+    newkids_array
+end  
+
+
+def map_to_double(number_array)
+    doubled_array =[]
+    count = 0 
+    while count < number_array.length do
+    doubled_array << number_array[count] *2
+    count +=1
+  end 
+     doubled_array
+end 
+ 
+ 
+def map_to_square(array)
+  squared_array =[]
+  count = 0 
+  while count < array.length do 
+        squared_array << array[count]  ** 2
+  end 
+    squared_array
+end 
+ 
+ ######################################
+def reduce_to_total(array,start_value=0)
+    total = start_value
+    count = 0 
+      while count < array.length do
+         total += array[count]
+         count += 1 
+      end
+   total
+end 
+ 
+ def reduce_to_all_true(the_array)
+      count = 0 
+      while count < the_array.length do
+        return false if the_array == 0 
+        count +=1  
+      end 
+      return true
+end 
+
+def reduce_to_any_true(array)
+  count = 0  
+  while count < array do
+    return true if array >= count  
+    count +=-1
+  end  
+return false  
+end
