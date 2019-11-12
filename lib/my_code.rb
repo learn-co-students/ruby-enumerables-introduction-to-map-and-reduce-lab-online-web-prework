366397167b7658d5894203a972681f001d3cb8be
thomas dubiel <td1729@hotmail.com>
2019-11-12 01:09:33 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,51 @@
 # My Code here....
+def map_to_negativize(numbers)
+  new_array = []
+  numbers.each do |num|
+    neg_num = num * -1
+    new_array << neg_num
+  end
+  new_array
+end
+
+def map_to_no_change(set)
+  set.each do |num|
+    num
+  end
+end
+
+def map_to_double(set)
+  new_array = []
+  set.each do |num|
+    new_num = num * 2 
+    new_array << new_num
+  end 
+  new_array
+end
+
+def map_to_square(set)
+  new_array = []
+  set.each do |num|
+    new_num = num * num
+    new_array << new_num
+  end
+  new_array
+end
+
+def reduce_to_total(set, num=0)
+  total = num
+  set.each do |num|
+    total += num
+  end
+  total
+end
+
+def reduce_to_all_true(set)
+  set.all?
+end
+
+def reduce_to_any_true(source) 
+  source.any?
+end
+
+
