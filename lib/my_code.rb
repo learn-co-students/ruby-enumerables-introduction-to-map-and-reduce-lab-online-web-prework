bb5c4264995f42b8d43230703df5c6df4f825017
Almmond Hudnall <ahudnall24@gmail.com>
2019-11-28 08:21:37 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,43 @@
+map_to_negativize(-1[1,2,3])
+    =>[-1,-2,-3]
+
+map_to_no_change(1[1,2,3])
+  =>[1,2,3]
+
+
+
+
+map_to_double(2[1,2,3])
+  => [2,4,6]
+
+
+
+
+map_to_square(sqrt[1,4,9])
+  => [1,2,3]
+
+
+
+
+
+reduce_to_total(1[1,2,3],0)
+  => [6]
+
+
+reduce_to_all_true(2[2,4,8,16], only multiples of 2)
+  => [2,4,8,16]
+
+
+
+
+
+
+reduce_to_any_true(1[1,2,3], only odd integers)
+  => [1,3]
+
+
+
+
+
+
 # My Code here....
