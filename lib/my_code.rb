bfc63dbcb96ee84378284171670cc2a1d4776591
OLESYA MILLER <olesyam.miller@gmail.com>
2019-11-29 19:33:02 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,66 @@
 # My Code here....
+
+def map_to_negativize(source_array)
+  my_index = 0 
+  new_array = []
+  while my_index < source_array.length do 
+    new_array.push(source_array[my_index] * (-1))
+    my_index += 1 
+  end
+  new_array
+end
+
+def map_to_no_change(source_array)
+  source_array
+end
+
+def map_to_double(source_array)
+  my_index = 0 
+  new_array = []
+  while my_index < source_array.length do 
+    new_array.push(source_array[my_index] * 2)
+    my_index += 1 
+  end
+  new_array
+end
+
+def map_to_square(source_array)
+  my_index = 0 
+  new_array = []
+  while my_index < source_array.length do 
+    new_array.push(source_array[my_index] ** 2)
+    my_index += 1 
+  end
+  new_array
+end
+
+def reduce_to_total(source_array, starting_point = 0)
+  my_index = 0 
+  while my_index < source_array.length do 
+    starting_point += source_array[my_index] 
+    my_index += 1
+  end
+  starting_point 
+end
+
+def reduce_to_all_true(source_array)
+  #my_index = 0 
+  #while my_index < source_array.length do 
+    #if source_array[my_index] == false  
+      #false 
+    #end
+    #my_index += 1 
+  #end 
+  
+  source_array.all?
+end
+
+def reduce_to_any_true(source_array)
+  source_array.any?
+end
+
+
+
+
+ 
+
