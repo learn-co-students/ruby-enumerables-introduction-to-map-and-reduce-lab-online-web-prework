b1e5d16ac3726949b5324f117dbf2958a2371bff
OLESYA MILLER <olesyam.miller@gmail.com>
2019-12-01 06:27:34 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -44,19 +44,21 @@ def reduce_to_total(source_array, starting_point = 0)
 end
 
 def reduce_to_all_true(source_array)
-  #my_index = 0 
-  #while my_index < source_array.length do 
-    #if source_array[my_index] == false  
-      #false 
-    #end
-    #my_index += 1 
-  #end 
-  
-  source_array.all?
+  my_index = 0 
+  while my_index < source_array.length do 
+    return false if !source_array[my_index]
+    my_index += 1 
+  end 
+  return true 
 end
 
 def reduce_to_any_true(source_array)
-  source_array.any?
+  my_index = 0 
+  while my_index < source_array.length do 
+    return true if source_array[my_index]
+    my_index += 1 
+  end 
+  return false  
 end
 
 
