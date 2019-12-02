5ed73f9389fa23e5824f43a6de2ab45f0c7fad94
Anna Lewis Dagnino <annaick1995@gmail.com>
2019-12-02 16:16:49 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -72,7 +72,7 @@ def reduce_to_all_true(source_array)
   TRUE
 end 
 
-def reduce_to_any_true(source_array)i = 0
+def reduce_to_any_true(source_array)
   i = 0
   
   while i < source_array.length do 
