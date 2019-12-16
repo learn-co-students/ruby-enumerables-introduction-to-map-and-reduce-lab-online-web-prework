768cd0b010a6fddbc217c1030c9d4f35c0a89602
Dan Foley <dan@dan-foley.com>
2019-12-16 23:35:07 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -5,9 +5,10 @@ def map_to_negativize(array)
   new_array = []
   while i < array.length do
     new_array << array[i] * -1
+    i += 1
   end
   new_array
-  binding.pry
+  # binding.pry
 end
 
 def map_to_no_change
