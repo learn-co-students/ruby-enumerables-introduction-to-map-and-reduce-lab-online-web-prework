2664c9daae61bdfac7d03b731e7fe4f4254a5d9b
Dan Foley <dan@dan-foley.com>
2019-12-16 23:34:25 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Introduction to Map and Reduce Lab
+_n# Introduction to Map and Reduce Lab
 
 ## Learning Goals
 
--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,15 @@
+require 'pry'
 # My Code here....
+def map_to_negativize(array)
+  i = 0
+  new_array = []
+  while i < array.length do
+    new_array << array[i] * -1
+  end
+  new_array
+  binding.pry
+end
+
+def map_to_no_change
+
+end
